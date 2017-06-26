4bcb11c69e46d0262e762e122aa7cf7da294aadc
Caleb Middlebrook <cmiddlebrook@sae.au>
2017-06-27 09:11:01 +1000

    Aspect ratio script added

---
+++ Assets/Scripts/ResolutionScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionScript : MonoBehaviour
+{
+    [SerializeField]
+    private int scrHeight;
+    [SerializeField]
+    private int scrWidth;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Screen.SetResolution(scrWidth, scrHeight, false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
