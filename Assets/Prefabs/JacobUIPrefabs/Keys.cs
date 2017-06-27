a09dacd9a24573772d1842a09f011bc32280dba5
Jacob Duniam <duniamjj@gmail.com>
2017-06-27 11:12:01 +1000

    Login and key logic now works!

---
+++ Assets/Prefabs/JacobUIPrefabs/Keys.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keys : MonoBehaviour {
+	public static Keys instance;
+
+	//Store all info as a string
+	public string usernameStored = "_usernameStored";
+	public string passwordStored = "_passwordStored";
+	public string nameStored = "_nameStored";
+	public string ageStored = "_AgeStored";
+	public string isMale = "_isMale";
+
+	public string playerGrade = "_playerGrade";
+
+	public void Start()
+	{
+		instance = this;
+
+		//Make each of the variables null
+		if(!PlayerPrefs.HasKey(usernameStored))
+		{
+			PlayerPrefs.SetString(usernameStored, null);
+		}
+		if(!PlayerPrefs.HasKey(passwordStored))
+		{
+			PlayerPrefs.SetString(usernameStored, null);
+		}
+		if(!PlayerPrefs.HasKey(nameStored))
+		{
+			PlayerPrefs.SetString(nameStored, null);
+		}
+		if(!PlayerPrefs.HasKey(ageStored))
+		{
+			PlayerPrefs.SetInt(ageStored, 0);
+		}
+		if(!PlayerPrefs.HasKey(isMale))
+		{
+			PlayerPrefs.SetInt (isMale, 0);
+		}
+		if(!PlayerPrefs.HasKey(playerGrade))
+		{
+			PlayerPrefs.SetInt(playerGrade, 0);
+		}
+	}
+}
---
+++ Assets/UI_LoginLogic.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_LoginLogic : MonoBehaviour {
+	public Text usernameEntry;
+	public Text passwordEntry;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick()
+	{
+		//First, check if both text entries are full
+		if(usernameEntry.text != null || passwordEntry.text != null)
+		{
+			//Compare the username and password with their entries in PlayerPrefs
+			if(PlayerPrefs.HasKey(Keys.instance.usernameStored) && PlayerPrefs.HasKey(Keys.instance.passwordStored))
+			{
+				Debug.Log("username and password keys found!");
+				if(PlayerPrefs.GetString(Keys.instance.usernameStored) == usernameEntry.text)
+				{
+					Debug.Log("Username checks out");
+						
+						// Check the password
+						if(PlayerPrefs.GetString(Keys.instance.passwordStored) == passwordEntry.text)
+						{
+							Debug.Log("Password checks out");
+							Login();
+						}
+						else
+						{
+							Debug.Log("Password not recognised");
+						}
+				}
+				else
+				{
+					Debug.Log("Username doesn't match the database!");
+				}
+			}
+			else
+			{
+				Debug.Log("username and password keys not found!");
+			}
+		}
+		else
+		{
+			Debug.Log("Either Username or Password entries are empty!");
+		}
+	}
+
+	public void Login()
+	{
+		Debug.Log("Logging the player in!");
+	}
+}
