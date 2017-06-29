e3da1568138031c2e595de94ab6e8d20af82e9b6
Levi Stenton <1005973@student.SAE.edu.au>
2017-06-29 16:07:01 +1000

    -Secateur Game

--- Assets/Scripts/LeviScript.cs
+++ Assets/Scripts/LeviScript.cs
@@ -17,80 +17,69 @@ public class LeviScript : MonoBehaviour {
     public Toggle taskFour;
     public Toggle taskFive;
 
+    public bool collectedSecateurs;
+    public GameObject secateurs;
+
     // Use this for initialization
     void Start()
     {
-       
+        collectedSecateurs = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         questComplete();
+
+        if (collectedSecateurs)
+        {
+            secateurs.SetActive(false);
+            taskOne.isOn = true;
+        }
     }
 
     public void gardenButtonPressed()
     {
 
     }
-
     public void questsButtonPressed()
     {
         scannerCanvas.SetActive(false);
         questsCanvas.SetActive(true);
     }
-
     public void scannerButtonPressed()
     {
         questsCanvas.SetActive(false);
         scannerCanvas.SetActive(true);
     }
-
     public void minigameButtonPressed()
     {
         scannerCanvas.SetActive(false);
         questsCanvas.SetActive(false);
         miniGameCanvas.SetActive(true);
     }
-
     public void exitMiniGameButton()
     {
         miniGameCanvas.SetActive(false);
         scannerCanvas.SetActive(true);
     }
-
-    public void taskOneComplete()
-    {
-        taskOne.isOn = true;
-    }
-
+        
     public void taskTwoComplete()
     {
         taskTwo.isOn = true;
     }
-
     public void taskThreeComplete()
     {
         taskThree.isOn = true;
-    }
-
-    public void taskFourComplete()
-    {
-        taskFour.isOn = true;
-    }
-    public void taskFiveComplete()
-    {
-        taskFive.isOn = true;
-    }
+    }  
 
     public void questComplete()
     {
-        if (taskOne.isOn == true && taskTwo.isOn == true && taskThree.isOn == true && taskFour.isOn == true && taskFive.isOn == true)
+        if (taskOne.isOn == true && taskTwo.isOn == true && taskThree.isOn == true)
         {
             questCompleteCanvas.SetActive(true);
         }
     }
-
     public void questCompleteClose()
     {
         questCompleteCanvas.SetActive(false);
@@ -99,6 +88,11 @@ public class LeviScript : MonoBehaviour {
         taskThree.isOn = false;
         taskFour.isOn = false;
         taskFive.isOn = false;
+    }
+
+    public void collectSecateurs()
+    {
+        collectedSecateurs = true; 
     }
 
 }
